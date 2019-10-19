f68ce43278288ccd604fd201b32dc44244beeea4
RickyyCoder <ricardopaul459@gmail.com>
2019-10-19 13:12:33 -0400

    UI set

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,27 @@
 import React from 'react'
-
-const ExpenseItem = () => {
+import {MdEdit, MdDelete} from 'react-icons/md'
+// {expense:{id,charge,amount}}
+ 
+const ExpenseItem = ({expense}) => {
+    // destructuring expense in single variable
+    const { id, charge, amount } = expense;
     return (
-        <div>
-            ExpenseItem
-        </div>
+        <li className="item">
+            <div className="info">
+                <span className="expense">{charge}</span>
+                <span className="amount">{amount}</span>
+            </div>
+
+            <div>
+                <button className="edit-button" aria-label="edit-button">
+                    <MdEdit />
+                </button>
+                <button className="edit-button" aria-label="edit-button">
+                    <MdDelete />
+                </button>
+            </div>
+
+        </li>
     )
 }
 
